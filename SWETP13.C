de0014c97c6e8814487c8cd7438ee306aa73af32
SwetZadafiya <136707833+SwetZadafiya@users.noreply.github.com>
2023-07-05 17:15:40 +0530

    Update SWETP13.C

---
+++ SWETP13.C
@@ -0,0 +1,24 @@
+/*1
+  2  3
+  4  5  6
+  7  8  9  10
+  11 12 13 14 15*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int a,b,c=1;
+
+clrscr();
+
+for(a=1;a<=5;a++){
+   for(b=1;b<=a;b++){
+      printf("%d  ",k++);
+   }
+printf("\n");
+}
+
+getch();
+}
